406ea7657e4ceb39485aae274ccb3a06b1d2cd20
Fernando Rodriguez <frodriguez.developer@outlook.com>
2015-07-29 17:07:41 -0400

    Added new policies

--- usr/lib/apparmor/apparmor-policy-load.sh
+++ usr/lib/apparmor/apparmor-policy-load.sh
@@ -2,4 +2,4 @@
 
 aa_profiles='/etc/apparmor.d/'
 aa_log='/var/log/apparmor.init.log'
-/sbin/apparmor_parser -r $(find "$aa_profiles" -maxdepth 1 -type f) #2>> "$aa_log"
+/sbin/apparmor_parser -r $(find "$aa_profiles" -maxdepth 1 -type f -not -path '*/\.*') 
