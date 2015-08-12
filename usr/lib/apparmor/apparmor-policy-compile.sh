44480ffe4ad57b5670b7bbce6be420de98125068
Fernando Rodriguez <frodriguez.developer@outlook.com>
2015-08-11 23:05:26 -0400

    Create cache directory when it doesn't exist

--- usr/lib/apparmor/apparmor-policy-compile.sh
+++ usr/lib/apparmor/apparmor-policy-compile.sh
@@ -4,5 +4,5 @@ aa_profiles='/etc/apparmor.d/'
 aa_log='/var/log/apparmor.init.log'
 PROFILES=`find "$aa_profiles" -maxdepth 1 -type f -not -path '*/\.*'`
 for profile in $PROFILES; do
-    apparmor_parser -QW "$profile" 2>> "$aa_log"
+    apparmor_parser --create-cache-dir -QW "$profile" 2>> "$aa_log"
 done
